c479d5c28bb1236425c3bfd1c915f3c5c6bfa277
Andrea Gonzato <gonzatoandrea98@gmail.com>
2022-06-14 10:03:17 +0200

    define p_c_k -collaborative work with alex

--- store.py
+++ store.py
@@ -1,5 +1,9 @@
+from locker_customer import LockerCustomer
+
+
 class Store:
     counter = 0  # this is a static value for the class
+    stores = []
 
     def __new__(cls, location, capacity, is_warehouse=False):
         obj = object.__new__(cls)
@@ -14,11 +18,26 @@ class Store:
         else:
             self.is_warehouse = False
         Store.counter += 1
+        Store.stores.append(self)
 
     def __repr__(self):
         if self.is_warehouse:
             return "W"
         else:
             return "L" + str(self.index)
+
+    def find_associated_CL(self, customers, stores):
+        associated_locker_customer = []
+        if not self.is_warehouse:
+            # only for locker
+            C_L = list(filter(lambda customer: type(customer) == LockerCustomer, customers))
+            for cl in C_L:
+                if cl.get_nearest_store(stores).index == self.index:
+                    associated_locker_customer.append(cl)
+        return associated_locker_customer
+
+
+
+
 
 
