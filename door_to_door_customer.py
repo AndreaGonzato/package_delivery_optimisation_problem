7d7ac958c87f56248ee85638d1b093f38914e833
Andrea Gonzato <gonzatoandrea98@gmail.com>
2022-06-15 11:12:52 +0200

    small fix and bugs -collaborative work with alex

--- door_to_door_customer.py
+++ door_to_door_customer.py
@@ -5,14 +5,13 @@ class DoorToDoorCustomer(Customer):
 
     door_to_door_customer_index = 0  # this is a static value for the class
 
-    def __new__(cls, location, package_demand=1, prime=False):
+    def __new__(cls, location, package_demand=1):
         obj = object.__new__(cls)
         return obj
 
-    def __init__(self, location, package_demand=1, prime=False):
+    def __init__(self, location, package_demand=1):
         super().__init__(location, package_demand)
         self.door_to_door_customer_index = DoorToDoorCustomer.door_to_door_customer_index
-        self.prime = prime
         DoorToDoorCustomer.door_to_door_customer_index += 1
 
     def __repr__(self):
